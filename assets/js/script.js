6363b790a6d48a8344ab65b6923a9a1958e6d9d8
alfidh02 <alfidarmawan79@gmail.com>
2024-07-17 18:05:48 +0700

    :bento: Update assets (images, js script)

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function getData(event) {
+  event.preventDefault();
+  let nameField = document.getElementById("nameInput").value;
+  let emailField = document.getElementById("emailInput").value;
+  let phoneField = document.getElementById("phoneInput").value;
+  let subjectField = document.getElementById("subject").value;
+  let messageField = document.getElementById("message").value;
+
+  console.log(nameField);
+  console.log(emailField);
+  console.log(phoneField);
+  console.log(subjectField);
+  console.log(messageField);
+}
