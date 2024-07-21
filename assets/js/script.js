c3eea358737d6435991e42c202fd0aaedb2bd915
alfidh02 <alfidarmawan79@gmail.com>
2024-07-21 11:56:12 +0700

    :sparkles: Add new feature : submit button sending to emai

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-function getData() {
+function getData(event) {
+  event.preventDefault();
   let nameField = document.getElementById("nameInput").value;
   let emailField = document.getElementById("emailInput").value;
   let phoneField = document.getElementById("phoneInput").value;
   let subjectField = document.getElementById("subject").value;
   let messageField = document.getElementById("message").value;
+
+  if (nameField == "") {
+    return alert("Tolong diisikan nama kamu");
+  } else if (emailField == "") {
+    return alert("Tolong diisikan email kamu");
+  }
 
   console.log(nameField);
   console.log(emailField);
@@ -16,4 +23,10 @@ function getData() {
   document.getElementById("phoneInput").value = "";
   document.getElementById("subject").value = "";
   document.getElementById("message").value = "";
+
+  let myEmail = "alfidarmawan79@gmail.com";
+  let subject = `Introduction - ${nameField}`;
+  let a = document.createElement("a");
+  a.href = `mailto:${myEmail}?subject=${subject}&body=Halo bang, nama saya ${nameField}, saya dari jurusan ${subjectField}, saya mau bilang ${messageField}`;
+  a.click();
 }
